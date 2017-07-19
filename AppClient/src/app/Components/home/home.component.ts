c6a21807b40edec8766a5270074af909a597eaa0
RabinShrestha <lovingspirit617@gmail.com>
2017-07-19 07:52:44 -0500

    user module initial commit

--- AppClient/src/app/Components/home/home.component.ts
+++ AppClient/src/app/Components/home/home.component.ts
@@ -4,11 +4,8 @@ import { Observable } from 'rxjs'
 
 import { PostServiceService } from '../../services/post-service.service';
 import { AuthService } from '../../services/auth/auth.service';
-<<<<<<< HEAD
 import { UserService } from '../../services/user/user.service';
-=======
 import { HomeService  } from '../../services/home.service';
->>>>>>> 61488b0745b70eadab0b651e7321e2bcf630e81b
 
 @Component({
   selector: 'app-home',
@@ -21,11 +18,8 @@ export class HomeComponent implements OnInit {
 
   private postData;
   private data;
-<<<<<<< HEAD
-  constructor(private service: PostServiceService, private auth: AuthService,private userservice:UserService) { }
-=======
-  constructor(private service: PostServiceService, private auth: AuthService, private homeService: HomeService) { }
->>>>>>> 61488b0745b70eadab0b651e7321e2bcf630e81b
+
+  constructor(private service: PostServiceService, private auth: AuthService, private homeService: HomeService,private userservice:UserService) { }
 
   ngOnInit() {
     this.auth.handleAuthentication();
